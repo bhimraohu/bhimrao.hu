faf81d01daccb78926e3ab59abd4de6fcbdfb526
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-11 12:21:17 +0200

    chore: update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,44 @@
+const { prismicRepo, defaultLanguage, langs } = require('./prismic-config')
+
 module.exports = {
   siteMetadata: {
     title: `Bhimrao`,
-    description: `Bhimrao.hu with Gatsby.`,
+    description: `Bhimrao egyesület honlapja.`,
     author: `Bhimrao`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: '@prismicio/gatsby-source-prismic-graphql',
+      options: {
+        repositoryName: prismicRepo,
+        defaultLang: defaultLanguage,
+        langs,
+        pages: [
+          {
+            type: 'homepage',
+            match: '/:lang?',
+            path: '/:lang?',
+            component: require.resolve('./src/templates/homepage.js'),
+            langs,
+          },
+          // {
+          //   type: 'page',
+          //   match: '/:lang?/:uid',
+          //   path: '/:lang?/:uid',
+          //   component: require.resolve('./src/templates/page.js'),
+          //   langs,
+          // },
+          // {
+          //   type: 'projects',
+          //   match: '/:lang?/projects/:uid',
+          //   path: '/:lang?/projects/:uid',
+          //   component: require.resolve('./src/templates/projects.js'),
+          //   langs,
+          // },
+        ],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -25,14 +59,6 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true, // defaults to false
-        jsxPragma: `jsx`, // defaults to "React"
-        allExtensions: true, // defaults to false
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
