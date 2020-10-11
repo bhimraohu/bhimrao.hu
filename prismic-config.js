c21bcaff58d3bfa834d0268d737bfde9d2d8e00f
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-11 02:06:26 +0200

    feat: add prismic config

---
+++ prismic-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // The repo name for your Prismic repository (found in the url of your repo: https://multilang-site-theme-test.prismic.io/)
+  prismicRepo: 'bhimraohu',
+
+  // Language configuration in your Prismic repository: Settings / Translations & locals
+  defaultLanguage: 'hu',
+  langs: ['hu', 'en-us'],
+}
