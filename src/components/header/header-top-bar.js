28e79265cb3415c331aaafa48f0dd161e15a7e99
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-15 00:51:38 +0200

    feat: add HeaderTopBar component

---
+++ src/components/header/header-top-bar.js
@@ -0,0 +1,77 @@
+import React from "react"
+import styled from "styled-components"
+
+const TopBarWrapper = styled.div`
+  padding: 1.05rem;
+  background-color: #1D1D3C;
+  
+  div {
+    margin: 0 auto;
+    max-width: 960px;
+  }
+
+  ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: flex-end;
+    margin: 0;
+    align-items: center;
+
+    li {
+      padding-left: 10px;
+      margin-bottom: 0;
+    }
+
+    .search {
+      padding-right: 50px;
+    }
+
+    .language {
+      padding-left: 50px;
+    }
+  }
+`;
+
+const Search = styled.input`
+`;
+
+const Button = styled.div`
+  padding: 10px 7px;
+  color: white;
+  background-color: transparent;
+  border: none;
+
+  &:hover {
+    background-color: red;
+    cursor: pointer;
+  }
+`;
+
+const HeaderTopBar = ({ topbarsData }) => {
+  return (
+    <TopBarWrapper>
+      <div>
+        <ul>
+          <li className="search">
+            <Search type="text" placeholder={topbarsData.search_placeholder} />
+          </li>
+          {
+            topbarsData.social_links.map((link, idx) => (
+              <li key={idx}>
+                <a href={link.link.url} title={link.tooltip} alt={link.alt_text} target="_blank" rel="noreferrer">X</a>
+              </li>
+            ))
+          }
+          <li className="language">
+            <Button>{topbarsData.language_main}</Button>
+          </li>
+          <li>
+            <Button>{topbarsData.language_second}</Button>
+          </li>
+        </ul>
+      </div>
+    </TopBarWrapper>
+  )
+}
+
+export default HeaderTopBar
