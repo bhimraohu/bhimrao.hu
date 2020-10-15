d9f5267a6b33760e43e79ff3da2666bf082138fd
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-16 01:30:58 +0200

    refactor: update Header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,24 +1,18 @@
 import React from "react"
+import styled from "styled-components"
 
 import HeaderTopBar from "./header-top-bar"
+import TopNavigation from "./top-navigation"
+
+const HeaderWrapper = styled.header`
+  margin-bottom: 1.05rem;
+`;
 
 const Header = ({ navigationData }) => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <HeaderWrapper>
     <HeaderTopBar topbarsData={navigationData.allHeader_topbars} />
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <div>Nav</div>
-    </div>
-  </header>
+    <TopNavigation navigationData={navigationData} />
+  </HeaderWrapper>
 )
 
 export default Header
