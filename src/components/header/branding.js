8c83307e6c3c478908cd8a96e1e9dfa8c529f275
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-18 01:56:38 +0200

    feat: create Branding component

---
+++ src/components/header/branding.js
@@ -0,0 +1,37 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+import { Colors } from "../../utils/constants";
+
+const BrandingWrapper = styled.div`
+  a {
+    text-decoration: none;
+    color: ${Colors.main};
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+  .logo {
+    height: 66px;
+  }
+`;
+
+const Branding = ({ navigationData }) => {
+  return (
+    <BrandingWrapper>
+      <Link to="/">
+        {
+          navigationData?.allHeader_navbars?.brand_logo_image?.url
+            ? <img
+              className="logo"
+              src={navigationData.allHeader_navbars.brand_logo_image.url}
+              alt={navigationData.allHeader_navbars.brand_logo_image.alt} />
+            : navigationData.allHeader_navbars.branding
+        }
+      </Link>
+    </BrandingWrapper>
+  )
+};
+
+export default Branding;
