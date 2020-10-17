565f522f5a356dc71efdca2dde4ac2659ae5c3af
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-17 18:54:57 +0200

    feat: add color constants

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+/* Use the same values in static/styles/common.css :root */
+export const Colors = {
+  main: '#1D1D3C',
+  red: '#D44F3B',
+  dirtyWhite: '#EEEEEE',
+  textColor: '#5D5D73',
+  headerColor: '#343450',
+}
