5cfde0a6f2d481fce690b0f1a5e372e00259caa5
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-22 01:44:02 +0200

    refactor: rename slices folder to homepage-slices

--- src/templates/homepage.js
+++ src/templates/homepage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout/Layout"
-import SliceZone from "../components/slices/SliceZone"
+import SliceZone from "../components/homepage-slices/SliceZone"
 
 class Homepage extends React.Component {
 
