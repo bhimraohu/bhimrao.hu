2e260dde065a93b6c3ab75b76bbfd661bcc268f5
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-22 01:57:32 +0200

    feat: create IntroText slice

---
+++ src/components/homepage-slices/IntroText.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import RichTextCustom from '../common/richtext';
+import { DesignSettings } from "../../utils/constants";
+
+const IntroTextWrapper = styled.section`
+  margin: 0 auto;
+  width: ${DesignSettings.innerWidth};
+`;
+
+const IntroText = ({ title, text }) => {
+
+  return (
+    <IntroTextWrapper>
+      <div className="h1-border-bottom">
+        <RichTextCustom render={title} />
+        <RichTextCustom render={text} />
+      </div>
+    </IntroTextWrapper>
+  );
+}
+
+export default IntroText;
