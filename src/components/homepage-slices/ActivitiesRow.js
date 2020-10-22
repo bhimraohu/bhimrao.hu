ad145e199cddb4ae5eaf9b6f8588ff7bf391f56b
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-23 01:01:09 +0200

    feat: create ActivitiesRow slice

---
+++ src/components/homepage-slices/ActivitiesRow.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Colors } from "../../utils/constants";
+
+const ActivitiesRowWrapper = styled.section`
+  width: 100%;
+  display: flex;
+  height: 25rem;
+
+  .item {
+    display: flex;
+    flex: 0 0 25%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .item:nth-of-type(odd) {
+    background: ${Colors.imageRowBackground};
+  }
+  .item:nth-of-type(even) {
+    background: ${Colors.imageRowBackground2};
+  }
+
+
+  .item:nth-of-type(1) .item-icon {
+    width: 7.3rem;
+    height: 7.3rem;
+  }
+  .item:nth-of-type(2) .item-icon {
+    width: 9.2rem;
+    height: 7.3rem;
+  }
+  .item:nth-of-type(3) .item-icon {
+    width: 8.6rem;
+    height: 7.3rem;
+  }
+  .item:nth-of-type(4) .item-icon {
+    width: 8.8rem;
+    height: 7.3rem;
+  }
+
+  .item-text {
+    text-align: center;
+    font-weight: bold;
+    font-size: 2rem;
+  }
+`;
+
+const ActivitiesRow = ({ fields }) => {
+
+  return (
+    <ActivitiesRowWrapper>
+      {
+        fields.map((field) => {
+          return (
+            <div className="item">
+              <img className="item-icon" src={field.activity_icon.url} alt={field.activity_label} />
+              <span className="item-text" >{field.activity_label}</span>
+            </div>
+          )
+        })
+      }
+    </ActivitiesRowWrapper>
+  );
+}
+
+export default ActivitiesRow;
