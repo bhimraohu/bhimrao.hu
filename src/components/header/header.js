5a965fd268b0a5454e4bd0bda00740c72e8f3971
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-23 17:17:43 +0200

    style: add margin bottom to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,6 +5,7 @@ import HeaderTopBar from "./header-top-bar"
 import TopNavigation from "./top-navigation"
 
 const HeaderWrapper = styled.header`
+  margin-bottom: 1.5rem; 
   box-shadow: 0px 0px 3px black;
 `;
 
