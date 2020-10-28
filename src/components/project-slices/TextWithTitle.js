93eab6aacb739f4d9a0d5eba05a7efe94124225e
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-28 22:47:13 +0100

    feat: create TextWithTitle component

---
+++ src/components/project-slices/TextWithTitle.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import RichTextCustom from '../common/richtext';
+import { DesignSettings } from "../../utils/constants";
+
+const TextWithTitleWrapper = styled.section`
+  margin: 5rem auto;
+  width: ${DesignSettings.innerWidth};
+
+  .h1-border-bottom > h1,
+  .title-wrapper > .h1-border-bottom > h1 {
+    font-size: 2.8rem;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+
+  .content-wrapper > p {
+    font-size: 2rem;
+    margin: 2rem 0;
+  }
+`;
+
+const TextWithTitle = ({ text_title, text_subtitle, text }) => {
+
+  return (
+    <TextWithTitleWrapper>
+      <div className="h1-border-bottom">
+        <div className="title-wrapper h1-border-bottom">
+          <RichTextCustom render={text_title} />
+        </div>
+
+        <div className="content-wrapper">
+          <RichTextCustom render={text_subtitle} />
+        </div>
+
+        <div className="content-wrapper">
+          <RichTextCustom render={text} />
+        </div>
+      </div>
+    </TextWithTitleWrapper>
+  );
+}
+
+export default TextWithTitle;
