1e889a162f8ce73f5c9312cebd8fd2d9e9de11b5
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-28 23:17:20 +0100

    feat: create TextOnly component

---
+++ src/components/project-slices/TextOnly.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import RichTextCustom from '../common/richtext';
+import { DesignSettings } from "../../utils/constants";
+
+const TextOnlyWrapper = styled.section`
+  margin: 5rem auto;
+  width: ${DesignSettings.innerWidth};
+
+  .content-wrapper > p {
+    font-size: 2rem;
+    margin: 2rem 0;
+  }
+`;
+
+const TextOnly = ({ text }) => {
+
+  return (
+    <TextOnlyWrapper>
+      <div className="content-wrapper">
+        <RichTextCustom render={text} />
+      </div>
+    </TextOnlyWrapper>
+  );
+}
+
+export default TextOnly;
