56b9c0feb017594d60327a39318d21363d062c8c
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-28 22:47:28 +0100

    feat: create HighlightedText component

---
+++ src/components/project-slices/HighlightedText.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import RichTextCustom from '../common/richtext';
+import { Colors, DesignSettings } from "../../utils/constants";
+
+const HighlightedTextWrapper = styled.section`
+  margin: 5rem auto;
+  width: ${DesignSettings.innerWidth};
+
+  .h1-border-bottom > h1,
+  .title-wrapper > .h1-border-bottom > h1 {
+    font-size: 2.8rem;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+
+  
+
+  .highlighted-text {
+    margin: 4rem 0;
+  }
+
+  .highlighted-text > p {
+    border-left: 3px solid ${Colors.main};
+    padding-left: 3rem;
+    font-size: 2.4rem;
+    line-height: 3.5rem;
+  }
+`;
+
+const HighlightedText = ({ background_color, highlighted_text }) => {
+
+  return (
+    <HighlightedTextWrapper>
+      <div className="highlighted-text">
+        <RichTextCustom render={highlighted_text} />
+      </div>
+    </HighlightedTextWrapper>
+  );
+}
+
+export default HighlightedText;
