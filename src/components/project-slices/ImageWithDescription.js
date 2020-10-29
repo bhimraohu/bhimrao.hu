fa8e54266d5028b12ce68d49d9e8576fd5f07acc
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-29 01:17:22 +0100

    feat: create ImageWithDescription component

---
+++ src/components/project-slices/ImageWithDescription.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import RichTextCustom from '../common/richtext';
+import { Colors } from "../../utils/constants";
+import Icon from '../common/icon';
+
+const ImageWithDescriptionWrapper = styled.section`
+
+  .content-wrapper  {
+    font-size: 2rem;
+    margin: 2rem 0;
+    display: flex;
+    flex-direction: column;
+
+    .row {
+      display: flex;
+      flex-direction: row;
+
+      > div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 50%;
+      }
+
+      &.a > div > p {
+        color: #DEDBCB;
+      }
+
+      &.a > div i:before {
+        color: #DEDBCB;
+        font-size: 3rem;
+      }
+
+      &.b > div i:before {
+        font-size: 3rem;
+      }
+    }
+  }
+`;
+
+const TextContainer = styled.div`
+  background-color: ${props => props.background_color};
+  padding: 0 5rem;
+  text-transform: uppercase;
+`;
+
+const ImageWithDescription = ({ background_color_a, background_color_b, description_a, description_b, icon_a, icon_b, image_a, image_b }) => {
+
+  return (
+    <ImageWithDescriptionWrapper >
+      <div className="content-wrapper">
+        <div className="row a">
+          {getImage(image_a)}
+          {getText(background_color_a, description_a, icon_a)}
+        </div>
+        <div className="row b">
+          {getText(background_color_b, description_b, icon_b)}
+          {getImage(image_b)}
+        </div>
+      </div>
+    </ImageWithDescriptionWrapper>
+  );
+}
+
+const getImage = (image) => {
+  return (
+    <div>
+      <img src={image.url} alt={image.alt} />
+    </div>
+  )
+}
+
+const getText = (background_color, description) => {
+  return (
+    <TextContainer background_color={background_color}>
+      <Icon icon_class={'icon-quote'} color={Colors.main} />
+      <RichTextCustom render={description} />
+    </TextContainer>
+  )
+}
+
+export default ImageWithDescription;
