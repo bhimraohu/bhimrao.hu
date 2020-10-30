c131bb6e80bca4cbc939f4cbe5675013121922eb
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-10-30 01:57:04 +0100

    feat: create FullWidthQuote component

---
+++ src/components/project-slices/FullWidthQuote.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import RichTextCustom from '../common/RichTextCustom';
+import { Colors, DesignSettings } from "../../utils/constants";
+import Icon from '../common/Icon';
+
+const FullWidthQuoteWrapper = styled.section`
+  width: 100%;
+  background: ${Colors.main};
+  
+  /* .content-wrapper > p {
+    font-size: 2rem;
+    margin: 2rem 0;
+  } */
+  `;
+
+const TextContainer = styled.div`
+  margin: 5rem auto;
+  width: ${DesignSettings.innerWidth};
+  padding: 5rem 0;
+  text-transform: uppercase;
+  
+  > p {
+    color: ${Colors.dirtyWhite} !important;
+    font-size: 2.4rem;
+  }
+`;
+
+const FullWidthQuote = ({ text }) => {
+  return (
+    <FullWidthQuoteWrapper>
+      <div className="content-wrapper">
+        <TextContainer>
+          <Icon icon_class={'icon-quote'} color={Colors.dirtyWhite} size={'3rem'} />
+          <RichTextCustom render={text} />
+        </TextContainer>
+      </div>
+    </FullWidthQuoteWrapper>
+  );
+}
+
+export default FullWidthQuote;
