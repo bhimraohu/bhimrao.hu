19d47282f8d9122e71bbda4695eead6b281ae036
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-11-01 00:21:55 +0100

    refactor: use components in Activities

--- src/components/about-us-slices/Activities.js
+++ src/components/about-us-slices/Activities.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import RichTextCustom from '../common/RichTextCustom';
 import { Colors, DesignSettings } from "../../utils/constants";
+import TextWithTitle from '../common/TextWithTitle';
+import HighlightedText from '../common/HighlightedText';
+import TextOnly from '../common/TextOnly';
 
 const ActivitiesWrapper = styled.section`
   margin: 3rem auto;
-  width: ${DesignSettings.innerWidth};
-
-  .h1-border-bottom > h1,
-  .title-wrapper > .h1-border-bottom > h1 {
-    font-size: 2.8rem;
-    font-weight: bold;
-    text-transform: uppercase;
-  }
-
-  .content-wrapper > p {
-    font-size: 2rem;
-    margin: 2rem 0;
-  }
-
-  .highlighted-text {
-    margin: 4rem 0;
-  }
-
-  .highlighted-text > p {
-    border-left: 3px solid ${Colors.main};
-    padding-left: 3rem;
-    font-size: 2.4rem;
-    line-height: 3.5rem;
-  }
+  width: ${DesignSettings.textWidth};
+  margin-bottom: 5rem;
 
   .button-wrapper {
     display: flex;
@@ -62,21 +42,18 @@ const Activities = ({ title, content, highlighted_text, content_part_2, link, li
   return (
     <ActivitiesWrapper>
       <div className="h1-border-bottom">
-        <div className="title-wrapper h1-border-bottom">
-          <RichTextCustom render={title} />
-        </div>
+        <TextWithTitle
+          title={title}
+          text={content}
+        />
 
-        <div className="content-wrapper">
-          <RichTextCustom render={content} />
-        </div>
+        <HighlightedText
+          highlighted_text={highlighted_text}
+        />
 
-        <div className="highlighted-text">
-          <RichTextCustom render={highlighted_text} />
-        </div>
-
-        <div className="content-wrapper">
-          <RichTextCustom render={content_part_2} />
-        </div>
+        <TextOnly
+          text={content_part_2}
+        />
 
         <div className="button-wrapper">
           <a className="button" href={link.url} target="_blank" alt={link_label} rel="noreferrer">{link_label}&gt;</a>
