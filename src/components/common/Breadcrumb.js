05e25c6a1f14ec040e7107945193b92f6f6d5724
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-11-03 21:42:58 +0100

    feat: create Breadcrumb cmp

---
+++ src/components/common/Breadcrumb.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from "gatsby"
+
+import { Colors, DesignSettings } from "../../utils/constants";
+import { linkResolverBase } from '../../utils/linkResolverBase';
+
+const BreadcrumbWrapper = styled.section`
+  margin: 3rem auto;
+  width: ${DesignSettings.outerWidth};
+
+  .item-link {
+    font-size: 2rem;
+    font-weight: bold;
+    margin: 2rem 0;
+    text-decoration: none;
+    color: ${Colors.textColor};
+    cursor: pointer;
+
+    &.red {
+      color: ${Colors.red};
+    }
+  }
+`;
+
+const Breadcrumb = ({ fields }) => {
+
+  return (
+    <BreadcrumbWrapper>
+      {
+        fields.map((field, idx, arr) => {
+          console.log(field)
+          return arr.length - 1 !== idx
+            ? getCrumb(field, idx)
+            : getLastCrumb(field, idx)
+        })
+      }
+    </BreadcrumbWrapper>
+  );
+}
+
+const getCrumb = (field, idx) => {
+  return (
+    <Link
+      key={idx}
+      to={linkResolverBase(field.link._meta)}
+      className="item-link"
+    >
+      <span>{field.label} &gt; </span>
+    </Link>
+  )
+}
+
+const getLastCrumb = (field, idx) => {
+  return (
+    <Link
+      key={idx}
+      to={linkResolverBase(field.link._meta)}
+      className="item-link red"
+    >
+      <span>{field.label}</span>
+    </Link>
+  )
+}
+
+export default Breadcrumb;
