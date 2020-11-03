83f58b21ad4792becf03a0e933386c4a94a81b5b
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-11-03 21:36:32 +0100

    chore: create StudyHall template

---
+++ src/templates/study_hall.js
@@ -0,0 +1,230 @@
+import React from 'react';
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout/LayoutCmp"
+
+class StudyHall extends React.Component {
+
+  getNavigationData = (_props) => {
+    return {
+      allHeader_topbars: _props.data?.prismic?.allHeader_topbars.edges[0].node,
+      allHeader_navbars: _props.data?.prismic?.allHeader_navbars.edges[0].node,
+      allHeader_navigation_submenus: _props.data?.prismic?.allHeader_navigation_submenus,
+      allFooters: _props.data?.prismic?.allFooters.edges[0].node,
+    }
+  };
+
+  render() {
+    const navigationData = this.getNavigationData(this.props);
+
+    return (
+      <Layout navigationData={navigationData}>
+        {/* <SliceZone body={this.props.data.prismic.allStudy_halls.edges[0].node} /> */}
+      </Layout >
+    );
+  }
+}
+
+export default StudyHall;
+
+export const query = graphql`
+query studyHallQuery($lang: String) {
+  prismic {
+
+    allStudy_halls(lang: "hu") {
+      edges {
+        node {
+          title
+          links {
+            label
+            selected
+            link {
+              ... on PRISMIC_Study_hall {
+                _meta {
+                  uid
+                  type
+                  lang
+                }
+              }
+            }
+          }
+          thumbnails {
+            label
+            link {
+              ... on PRISMIC_Projects {
+                _meta {
+                  uid
+                  type
+                  lang
+                }
+              }
+              ... on PRISMIC_Project {
+                _meta {
+                  uid
+                  type
+                  lang
+                }
+              }
+              ... on PRISMIC_Study_hall {
+                _meta {
+                  lang
+                  type
+                  uid
+                }
+              }
+            }
+          }
+          text_part_1
+          text_part_2
+          text_part_3
+          image
+          image_collage {
+            big_image_position
+            collage_description
+            collage_title
+            description_position
+            image_big
+            image_small
+          }
+          image_row {
+            image_left
+            image_center
+            image_right
+          }
+        }
+      }
+    }
+    
+    allHeader_topbars (lang: $lang) {
+      edges {
+        node {
+          _meta {
+            lang
+            alternateLanguages {
+              lang
+            }
+          }
+          language_main
+          language_second
+          multilanguage_enabled
+          search_placeholder
+          social_links {
+            icon_class
+            tooltip
+            alt_text  
+            link {
+              ... on PRISMIC__ExternalLink {
+                url
+              }
+            }
+          }
+          address
+          address_label
+          address_maps_link
+        }
+      }
+    }
+
+    allHeader_navbars (lang: $lang) {
+      edges {
+        node {
+          brand_logo_image
+          branding
+          navigation_links {
+            label
+            submenu
+            is_cta
+            link {
+              ... on PRISMIC_About_us {
+                _meta {
+                  uid
+                  type
+                  lang
+                }
+              }
+              ... on PRISMIC_News {
+                _meta {
+                  uid
+                  type
+                  lang
+                }
+              }
+              ... on PRISMIC_Contact_page {
+                _meta {
+                  uid
+                  type
+                  lang
+                }
+              }
+              ... on PRISMIC_Help_us {
+                _meta {
+                  uid
+                  type
+                  lang
+                }
+              }
+              ... on PRISMIC_Projects {
+                _meta {
+                  uid
+                  type
+                  lang
+                }
+              }
+              ... on PRISMIC_Team {
+                _meta {
+                  uid
+                  type
+                  lang
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    allHeader_navigation_submenus(lang: $lang) {
+      edges {
+        node {
+          parent
+          sub_navigation_links {
+            label
+            link {
+              ... on PRISMIC_Project {
+                _meta {
+                  uid
+                  type
+                  lang
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    allFooters (lang: $lang) {
+      edges {
+        node {
+          first_section_title
+          first_section_links {
+            label
+            link {
+              ... on PRISMIC__ExternalLink {
+                url
+              }
+            }
+          }
+          second_section_title
+          second_section_field
+          third_section_title
+          supporter_logo
+          supporter_text
+          copyright_text
+          eu_logo
+        }
+      }
+    }
+  }
+}
+`;
