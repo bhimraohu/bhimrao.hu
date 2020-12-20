9f71c249d29207f68c51bb19c34668cc76e87eda
Laszlo Sliz <laszlo.sliz.biz@gmail.com>
2020-12-20 23:41:48 +0100

    fix: check if document exists

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -96,8 +96,9 @@ const Modal = ({ children, title, content, modalCloseTimeout, setToClose }) => {
 
   const [modalOpen, setModalOpen] = useState(true);
 
-
-  document.querySelector("html").style.overflow = 'hidden';
+  if (typeof document === 'object') {
+    document.querySelector('html').style.overflow = 'hidden';
+  }
 
   if (modalCloseTimeout) {
     const timeout = setTimeout(() => {
@@ -116,7 +117,9 @@ const Modal = ({ children, title, content, modalCloseTimeout, setToClose }) => {
 
   const close = () => {
     setModalOpen(false);
-    document.querySelector("html").style.overflow = 'visible';
+    if (typeof document === 'object') {
+      document.querySelector('html').style.overflow = 'visible';
+    }
     setToClose();
   }
 
@@ -153,26 +156,5 @@ const Modal = ({ children, title, content, modalCloseTimeout, setToClose }) => {
     </ModalWrapper>
   );
 }
-
-// const showDialog = () => {
-//   document.getElementById('dialog').classList.add('show')
-//   const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
-//   const body = document.body;
-//   body.style.position = 'fixed';
-//   body.style.top = `-${scrollY}`;
-// };
-// const closeDialog = () => {
-//   const body = document.body;
-//   const scrollY = body.style.top;
-//   body.style.position = '';
-//   body.style.top = '';
-//   window.scrollTo(0, parseInt(scrollY || '0') * -1);
-//   document.getElementById('dialog').classList.remove('show');
-// }
-// if (typeof window === 'object') {
-//   window?.addEventListener('scroll', () => {
-//     document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
-//   });
-// }
 
 export default Modal;
